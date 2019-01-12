32d8fa937283a221722fa5e2f62ed457bd6a1bdd
BohdanRakov <rakovbogdan@gmail.com>
2019-01-12 19:40:32 +0200

    CodeWriter arithmetic implementation

--- src/main/java/org/bohdanrakov/vmtranslator/CodeWriter.java
+++ src/main/java/org/bohdanrakov/vmtranslator/CodeWriter.java
@@ -1,14 +1,51 @@
 package org.bohdanrakov.vmtranslator;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class CodeWriter {
+
+    private static final String TXT_EXTENSION = ".txt";
+    private static final String UTF_8 = "UTF-8";
+    private static Map<String, List<String>> commands;
+
+    private static final String ADD = "add";
+    private static final String SUB = "sub";
+    private static final String EQ = "eq";
+    private static final String LT = "lt";
+    private static final String GT = "gt";
+    private static final String AND = "and";
+    private static final String OR = "or";
+    private static final String NEG = "neg";
+    private static final String NOT = "not";
+
+    static {
+        commands = new HashMap<>();
+        Stream.of(ADD, SUB, EQ, LT, GT, AND, OR, NEG, NOT).forEach(CodeWriter::addCommandToMap);
+    }
+
+    private static void addCommandToMap(String commandKey) {
+        ClassLoader classLoader = CodeWriter.class.getClassLoader();
+        try {
+            List<String> asmInstructions = IOUtils.readLines(
+                    classLoader.getResourceAsStream(commandKey + TXT_EXTENSION), UTF_8);
+            commands.put(commandKey, asmInstructions);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private List<String> result = new ArrayList<>();
 
     public void writeArithmetic(String command) {
-
+        List<String> asmInstructions = commands.get(command);
+        result.addAll(asmInstructions);
     }
 
     public void writePushPop(String memorySegment, int index) {
---
+++ src/test/java/org/bohdanrakov/vmtranslator/CodeWriterTest.java
@@ -0,0 +1,44 @@
+package org.bohdanrakov.vmtranslator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CodeWriterTest {
+
+    private CodeWriter testInstance;
+
+    @Test
+    public void testArithmetic() {
+        List<String> expectedAdd = new ArrayList<>();
+        expectedAdd.add("@SP");
+        expectedAdd.add("M=M-1");
+        expectedAdd.add("A=M");
+        expectedAdd.add("D=M");
+        expectedAdd.add("@R13");
+        expectedAdd.add("M=D");
+        expectedAdd.add("@SP");
+        expectedAdd.add("M=M-1");
+        expectedAdd.add("A=M");
+        expectedAdd.add("D=M");
+        expectedAdd.add("@R13");
+        expectedAdd.add("D=D+M");
+        expectedAdd.add("@SP");
+        expectedAdd.add("A=M");
+        expectedAdd.add("M=D");
+        expectedAdd.add("@SP");
+        expectedAdd.add("M=M+1");
+
+        testInstance = new CodeWriter();
+        testInstance.writeArithmetic("add");
+
+        assertEquals(expectedAdd, testInstance.getResult());
+
+    }
+}
